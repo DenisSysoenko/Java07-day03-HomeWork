c3b1e567dfcf1fbe1bc0d359b076e04995e947a5
Daniil <38988408+TIE-Bear@users.noreply.github.com>
2018-08-15 23:32:08 +0600

    Update HomeWork02
    Add Users and UserAccounts objects and call their methods.

--- src/main/java/org/andestech/learning/rfb18/HomeWork02.java
+++ src/main/java/org/andestech/learning/rfb18/HomeWork02.java
@@ -5,20 +5,16 @@ public class HomeWork02
 {
     public static void main( String[] args )
     {
-/*
-*
-* TODO:
-*
-* Создать интерфейс IAccount с методами
-* withdrawal, checkTransaction, put, report.
-*
-* Создать классы User, UserAccount (реализующий интерфейс IAccount ).
-* Добавить необходимые поля и методы для работы счета.
-* Произвести в методе main проверку: создать экземпляры User, UserAccount,
-* положить на счёт некую сумму, проверить лимиты, снять сумму, вывести отчёт операций.
- *
-* */
-
-
+        User Boris = new User ("Boris","Blade");
+        User Tirion = new User ("Tirion", "Lanister");
+        UserAccount ac0 = new UserAccount(Boris.getUserName(), 1000, Boris.getUserID());
+        UserAccount ac1 = new UserAccount(Tirion.getUserName(), 1000000, Tirion.getUserID());
+        ac0.put(1500);
+        ac0.withdrawal(578);
+        ac0.report();
+        ac1.put(15000);
+        ac1.withdrawal(98797);
+        ac1.report(); 
     }
 }
+
