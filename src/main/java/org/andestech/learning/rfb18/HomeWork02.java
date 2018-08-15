3cfc4cfcd4be9f1652e140ed448bb84b64cc470b
a0z9 <a0z9@rambler.ru>
2018-08-15 10:26:02 +0300

    2nd commit

--- src/main/java/org/andestech/learning/rfb18/HomeWork02.java
+++ src/main/java/org/andestech/learning/rfb18/HomeWork02.java
@@ -5,6 +5,20 @@ public class HomeWork02
 {
     public static void main( String[] args )
     {
+/*
+*
+* TODO:
+*
+* Создать интерфейс IAccount с методами
+* withdrawal, checkTransaction, put, report.
+*
+* Создать классы User, UserAccount (реализующий интерфейс IAccount ).
+* Добавить необходимые поля и методы для работы счета.
+* Произвести в методе main проверку: создать экземпляры User, UserAccount,
+* положить на счёт некую сумму, проверить лимиты, снять сумму, вывести отчёт операций.
+ *
+* */
+
 
     }
 }
