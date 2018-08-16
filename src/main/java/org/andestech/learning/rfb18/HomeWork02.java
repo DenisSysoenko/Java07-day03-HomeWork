736677302baeec0dc3b977180ff0dbe2d6a1d120
Daniil <38988408+TIE-Bear@users.noreply.github.com>
2018-08-16 09:38:34 +0600

    Small update
    Add fail withdrawal operation.

--- src/main/java/org/andestech/learning/rfb18/HomeWork02.java
+++ src/main/java/org/andestech/learning/rfb18/HomeWork02.java
@@ -11,10 +11,11 @@ public class HomeWork02
         UserAccount ac1 = new UserAccount(Tirion.getUserName(), 1000000, Tirion.getUserID());
         ac0.put(1500);
         ac0.withdrawal(578);
+        ac0.withdrawal(2050000);
         ac0.report();
         ac1.put(15000);
         ac1.withdrawal(98797);
-        ac1.report(); 
+        ac1.report();
     }
 }
 
