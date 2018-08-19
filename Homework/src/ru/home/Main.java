032823645c77f9dd3c0512c22bb19d1801322cc8
Artem Senko <r-tem92@mail.ru>
2018-08-19 21:22:04 +0600

    dz

--- Homework/src/ru/home/Main.java
+++ Homework/src/ru/home/Main.java
@@ -14,17 +14,4 @@ public class Main {
         userAccount.withdrawal(250);
         userAccount.report();
     }
-        /*
-    * TODO:
-    *
-    * Создать интерфейс IAccount с методами
-    * withdrawal, checkTransaction, put, report.
-    *
-    * Создать классы User, UserAccount (реализующий интерфейс IAccount ).
-    * Добавить необходимые поля и методы для работы счета.
-    * Произвести в методе main проверку: создать экземпляры User, UserAccount,
-    *положить на счёт некую сумму, проверить лимиты, снять сумму, вывести отчёт операций.
-    *
-     */
-
 }
